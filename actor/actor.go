2149eae484c0603d676c6ee6280288c80b50b407
Jack Chorley <jack.chorley16@imperial.ac.uk>
2018-11-09 10:51:20 +0000

    Initial actor class commit

---
+++ actor/actor.go
@@ -0,0 +1,10 @@
+package actor
+
+type Point struct {
+	x int
+	y int
+}
+
+type Actor struct {
+	CurrentLoc Point
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	a := actor.Actor{
+		CurrentLoc: {
+			x: 1,
+			y: 1,
+		},
+	}
+}
