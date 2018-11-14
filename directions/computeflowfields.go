99b4615c222a2b1ae72aedd5aed5c8b48966ee81
Thomas Pointon <thomas.pointon16@imperial.ac.uk>
2018-11-14 16:33:30 +0000

    restructured

--- directions/computeflowfields.go
+++ directions/computeflowfields.go
@@ -1,5 +1,7 @@
 package directions
 
+import "math"
+
 type Direction int
 
 const (
@@ -15,28 +17,31 @@ type Destination struct { // Indicies into the macromap
 }
 
 type Tile struct {
-	NextDirection Direction // The next direction someone should follow to go to the destination from this cell
-	Walkable      bool      // If this cell is a wall or not
-	RegionIds     []int     // The region ID's this tile is in
-	Dist          int       // This is only used internally for dijkstra
+	NextDirection Direction                 // The next direction someone should follow to go to the destination from this cell
+	Walkable      bool                      // If this cell is a wall or not
+	RegionIds     []int                     // The region ID's this tile is in
+	Dists         map[Destination]float64   // Used internally for dijkstra
+	Directions    map[Destination]Direction // Direction to destination
 }
 
 type MacroMap struct {
-	Width  int
-	Height int
-	tiles  [][]Tile // The tiles.
+	Width     int
+	Height    int
+	TileWidth float64  // Width of a tile
+	tiles     [][]Tile // The tiles.
+}
+
+func (mm *MacroMap) GetTileHighRes(x, y float64) (*Tile, error) {
+
+	return mm.GetTile(int(math.Floor(x/mm.TileWidth)), int(math.Floor(y/mm.TileWidth)))
+
 }
 
 func (mm *MacroMap) GetTile(x, y int) (*Tile, error) {
 	return &mm.tiles[y][x], nil
 }
 
-type FlowField MacroMap
-
-// Map of directions to their flow fields
-var FlowFields = make(map[Direction]*FlowField)
-
-func generateFlowField(macroMap MacroMap, destination Destination) (FlowField, error) {
+func generateFlowField(macroMap MacroMap, destination Destination) error {
 
 	//
 
